6989b6d634df829dedc62e73eaaac769bd4d8723
maycontainbrain <a.j.duszkiewicz@gmail.com>
2025-01-30 00:27:43 +0000

    Add compute_velocity function

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="woodcode",
+    version="0.1.0",
+    author="Adrian Duszkiewicz",
+    author_email="a.j.duszkiewicz@gmail.com",
+    description="A python package for neural data analysis in Wood/Dudchenko lab",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Wood-Dudchenko-lab/woodcode",
+    packages=find_packages(),  # Automatically finds packages inside the repo
+    install_requires=[  # Dependencies your package needs
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "pandas",
+        "pynapple==0.7.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
