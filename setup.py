cf7eb0122cc3d5017c5e6b4337914400327ad79d
maycontainbrain <a.j.duszkiewicz@gmail.com>
2025-02-03 23:07:31 +0000

    changed linear_track

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="woodcode",
-    version="0.1.0",
+    version="0.1.1",
     author="Wood/Dudchenko lab members",
     author_email="a.j.duszkiewicz@gmail.com",
     description="A python package for neural and behavioural data analysis in Wood/Dudchenko lab",
