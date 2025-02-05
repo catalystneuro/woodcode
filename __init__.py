8e3e51519574695a1dfea5a9ae92fb340590eea7
maycontainbrain <a.j.duszkiewicz@gmail.com>
2025-02-05 17:49:48 +0000

    changed structure

--- __init__.py
+++ __init__.py
@@ -1,1 +1,4 @@
-
+from . import general
+from . import place_cells
+from . import nwb
+from . import hd_cells
--- woodcode/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import general
-from . import place_cells
-from . import nwb
-from . import hd_cells
