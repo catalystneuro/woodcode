98c8d2e3900fbb8bfa4332556008e408382f9d5a
maycontainbrain <a.j.duszkiewicz@gmail.com>
2025-10-03 14:31:03 +0100

    Changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="woodcode",
-    version="0.1.1",
+    version="0.1.2",
     author="Wood/Dudchenko lab members",
     author_email="a.j.duszkiewicz@gmail.com",
     description="A python package for neural and behavioural data analysis in Wood/Dudchenko lab",
