1af04f229ef31c1dfd3c0e204a95534f549aa1c5
pauladkisson <paul.wesley.adkisson@gmail.com>
2026-03-17 07:43:00 -0700

    Added Placeholder for juvenile session aligned like an adult

--- moore_2025/convert_session.py
+++ moore_2025/convert_session.py
@@ -226,7 +226,7 @@ def main():
     juvenile_metadata_file_path = Path("/Users/pauladkisson/Documents/CatalystNeuro/DudchenkoConv/woodcode/moore_2025/juvenile_metadata.yaml")
     adult_metadata_file_path = Path("/Users/pauladkisson/Documents/CatalystNeuro/DudchenkoConv/woodcode/moore_2025/adult_metadata.yaml")
 
-    stub_test = False
+    stub_test = True
     if output_folder_path.exists():
         shutil.rmtree(output_folder_path)
     save_path = output_folder_path
@@ -500,42 +500,86 @@ def main():
     #     is_adult=False,
     # )
 
-    # Example Session without raw data
-    jv_wt_folder_path = juvenile_folder_path / "WT"
-    juvenile_histology_folder_path = histology_folder_path / "H3000"
-    folder_name = 'H3023-210812'
-    processed_xml_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.xml')  # path to xml file
-    raw_xml_path = processed_xml_path  # Raw data for this session is missing, so using the Processed XML instead
-    nrs_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.nrs')  # path to xml file
-    mat_path = jv_wt_folder_path / folder_name / "Processed" / 'Analysis'
-    sleep_path = jv_wt_folder_path / folder_name / "Processed" / 'Sleep'
-    video_file_paths = [
-        jv_wt_folder_path / folder_name / "Processed" / "BonsaiCaptureALL2021-08-12T19_44_12.avi",
-    ]
-    timestamps_file_paths = [
-        jv_wt_folder_path / folder_name / "Processed" / "BonsaiTracking.csv",
-    ]
-    lfp_file_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.lfp')
-    raw_ephys_dat_file_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.dat')  # Raw .dat file for this session since raw OpenEphys folder is missing
-    save_path = output_folder_path
-    session_to_nwb(
-        folder_name=folder_name,
-        raw_xml_path=raw_xml_path,
-        processed_xml_path=processed_xml_path,
-        nrs_path=nrs_path,
-        meta_path=meta_path,
-        mat_path=mat_path,
-        sleep_path=sleep_path,
-        video_file_paths=video_file_paths,
-        timestamps_file_paths=timestamps_file_paths,
-        lfp_file_path=lfp_file_path,
-        raw_ephys_dat_file_path=raw_ephys_dat_file_path,
-        save_path=save_path,
-        metadata_file_path=juvenile_metadata_file_path,
-        histology_folder_path=juvenile_histology_folder_path,
-        stub_test=stub_test,
-        is_adult=False,
-    )
+    # # Example Session without raw data
+    # jv_wt_folder_path = juvenile_folder_path / "WT"
+    # juvenile_histology_folder_path = histology_folder_path / "H3000"
+    # folder_name = 'H3023-210812'
+    # processed_xml_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.xml')  # path to xml file
+    # raw_xml_path = processed_xml_path  # Raw data for this session is missing, so using the Processed XML instead
+    # nrs_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.nrs')  # path to xml file
+    # mat_path = jv_wt_folder_path / folder_name / "Processed" / 'Analysis'
+    # sleep_path = jv_wt_folder_path / folder_name / "Processed" / 'Sleep'
+    # video_file_paths = [
+    #     jv_wt_folder_path / folder_name / "Processed" / "BonsaiCaptureALL2021-08-12T19_44_12.avi",
+    # ]
+    # timestamps_file_paths = [
+    #     jv_wt_folder_path / folder_name / "Processed" / "BonsaiTracking.csv",
+    # ]
+    # lfp_file_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.lfp')
+    # raw_ephys_dat_file_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.dat')  # Raw .dat file for this session since raw OpenEphys folder is missing
+    # save_path = output_folder_path
+    # session_to_nwb(
+    #     folder_name=folder_name,
+    #     raw_xml_path=raw_xml_path,
+    #     processed_xml_path=processed_xml_path,
+    #     nrs_path=nrs_path,
+    #     meta_path=meta_path,
+    #     mat_path=mat_path,
+    #     sleep_path=sleep_path,
+    #     video_file_paths=video_file_paths,
+    #     timestamps_file_paths=timestamps_file_paths,
+    #     lfp_file_path=lfp_file_path,
+    #     raw_ephys_dat_file_path=raw_ephys_dat_file_path,
+    #     save_path=save_path,
+    #     metadata_file_path=juvenile_metadata_file_path,
+    #     histology_folder_path=juvenile_histology_folder_path,
+    #     stub_test=stub_test,
+    #     is_adult=False,
+    # )
+
+    # # TODO: add this session once the .nrs file has been shared
+    # # Example Juvenile Session with Adult temporal alignment and H5 Probe
+    # jv_wt_folder_path = juvenile_folder_path / "WT"
+    # juvenile_histology_folder_path = histology_folder_path / "H3000"
+    # folder_name = 'H3029-230510'
+    # processed_xml_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.xml')  # path to xml file
+    # raw_xml_path = jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "recording1" / "continuous" / "Acquisition_Board-100.Rhythm Data" / "continuous.xml"
+    # nrs_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.nrs')  # path to xml file
+    # mat_path = jv_wt_folder_path / folder_name / "Processed" / 'Analysis'
+    # sleep_path = jv_wt_folder_path / folder_name / "Processed" / 'Sleep'
+    # video_file_paths = [
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiVideo2023-05-10T12_12_47.avi",
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiVideo2023-05-10T12_34_44.avi",
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiVideo2023-05-10T14_07_30.avi",
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiVideo2023-05-10T14_31_02.avi",
+    # ]
+    # timestamps_file_paths = [
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiTracking2023-05-10T12_12_45.csv",
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiTracking2023-05-10T12_34_42.csv",
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiTracking2023-05-10T14_07_28.csv",
+    #     jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2" / "BonsaiTracking2023-05-10T14_31_01.csv",
+    # ]
+    # lfp_file_path = jv_wt_folder_path / folder_name / "Processed" / (folder_name + '.lfp')
+    # raw_ephys_folder_path = jv_wt_folder_path / folder_name / "Raw" / "day2" / "experiment2"
+    # save_path = output_folder_path
+    # session_to_nwb(
+    #     folder_name=folder_name,
+    #     raw_xml_path=raw_xml_path,
+    #     processed_xml_path=processed_xml_path,
+    #     nrs_path=nrs_path,
+    #     meta_path=meta_path,
+    #     mat_path=mat_path,
+    #     sleep_path=sleep_path,
+    #     video_file_paths=video_file_paths,
+    #     timestamps_file_paths=timestamps_file_paths,
+    #     lfp_file_path=lfp_file_path,
+    #     raw_ephys_folder_path=raw_ephys_folder_path,
+    #     save_path=save_path,
+    #     metadata_file_path=juvenile_metadata_file_path,
+    #     histology_folder_path=juvenile_histology_folder_path,
+    #     stub_test=stub_test,
+    #     is_adult=True,
+    # )
 
 if __name__ == "__main__":
     main()
